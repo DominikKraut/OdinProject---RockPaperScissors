21d402bd877d47c8efadcb887b1355132f004ef9
Dominik Placr <dominik.placr@applifting.cz>
2022-11-03 15:12:56 +0100

    Added single-rounds function to play RockPaperScissors

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const getComputerChoice = () => {
 };
 
 console.log(getComputerChoice());
+
+const singleRound = (playerSelection, computerSelection) => {
+  computerSelection = getComputerChoice();
+  let player = playerSelection.toLowerCase();
+  if (player == computerSelection.toLowerCase()) {
+    return "It is a tie!" + " " + "The computer has chosen " + computerSelection;
+  }
+  if (player == "rock" && computerSelection == "Paper") {
+    return "You lose!" + " " + "The computer has chosen " + computerSelection;
+  }
+  if (player == "rock" && computerSelection == "Scissors") {
+    return "You win!" + "The computer has chosen " + computerSelection;
+  }
+  if (player == "paper" && computerSelection == "Scissors") {
+    return "You lose!" + "The computer has chosen " + computerSelection;
+  }
+  if (player == "paper" && computerSelection == "Rock") {
+    return "You win!" + "The computer has chosen " + computerSelection;
+  }
+  if (player == "scissors" && computerSelection == "Rock") {
+    return "You lose!" + "The computer has chosen " + computerSelection;
+  }
+  if (player == "scissors" && computerSelection == "Paper") {
+    return "You win!" + "The computer has chosen " + computerSelection;
+  }
+};
+
+console.log(singleRound("ScissorS"));
